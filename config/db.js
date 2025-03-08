9cd17b21246fc37f9a83f21e8ba4ed8afe91c680
CodewithMK180105 <manishkumar180105@gmail.com>
2025-03-08 09:50:57 +0530

    Some bugs fixes of process.env.DB_NAME and .js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB= async()=>{
     try{
-        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch(error){
         console.log("MongoDB connection Error");
