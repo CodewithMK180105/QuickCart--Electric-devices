b8a7c41349b87567fadc01c522c3f3db6b47c7d5
CodewithMK180105 <manishkumar180105@gmail.com>
2025-03-23 17:42:27 +0530

    Cart items coonected to the database.

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,5 +1,4 @@
 'use client'
-import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useAuth, useUser } from "@clerk/nextjs";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -76,7 +75,16 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = 1;
         }
         setCartItems(cartData);
-
+        
+        if(user){
+            try{
+                const token=await getToken();
+                await axios.post('/api/cart/update', {cartData}, {headers: {Authorization: `Bearer ${token}`}});
+                toast.success("Item added to cart");
+            } catch(error){
+                toast.error(error.message);
+            }
+        }
     }
 
     const updateCartQuantity = async (itemId, quantity) => {
@@ -88,7 +96,15 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = quantity;
         }
         setCartItems(cartData)
-
+        if(user){
+            try{
+                const token=await getToken();
+                await axios.post('/api/cart/update', {cartData}, {headers: {Authorization: `Bearer ${token}`}});
+                toast.success("Cart Updated");
+            } catch(error){
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartCount = () => {
