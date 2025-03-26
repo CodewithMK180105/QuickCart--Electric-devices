6c3a546530d24a3121d61c6d13178457c22c3a88
CodewithMK180105 <manishkumar180105@gmail.com>
2025-03-26 15:35:45 +0530

    Orders Placed stored in the database successfully.

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,11 +1,11 @@
-import { addressDummyData } from "@/assets/assets";
 import { useAppContext } from "@/context/AppContext";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const OrderSummary = () => {
 
-  const { currency, router, getCartCount, getCartAmount, getToken, user } = useAppContext()
+  const { currency, router, getCartCount, getCartAmount, getToken, user, cartItems, setCartItems } = useAppContext()
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -34,7 +34,43 @@ const OrderSummary = () => {
   };
 
   const createOrder = async () => {
+    try{
 
+      if(!selectedAddress){
+        return toast.error("Please select an Address");
+      }
+
+      let cartItemsArray=Object.keys(cartItems).map((key)=>({product: key, quantity: cartItems[key]}));
+      cartItemsArray=cartItemsArray.filter(item=>item.quantity>0);
+
+      if(cartItemsArray.length===0){
+        return toast.error("Cart is Empty");
+      }
+
+      const token=await getToken();
+      const {data}=await axios.post('/api/order/create', 
+        {
+          address: selectedAddress._id,
+          items: cartItemsArray,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      )
+
+      if(data.success){
+        toast.success(data.message);
+        setCartItems({});
+        router.push("/order-placed");
+      } else{
+        toast.error(data.message);
+      }
+
+    } catch(error){
+      toast.error("Hello");
+    }
   }
 
   useEffect(() => {
